a4bebdda99c720a8db9f234dcb289e079a225607
unknown <buuvo95@gmail.com>
2023-02-05 23:00:15 +0700

    Inter-Service Communication using WebClient

--- inventory-service/src/main/java/com/microservice/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/microservice/inventoryservice/service/InventoryService.java
@@ -1,9 +1,13 @@
 package com.microservice.inventoryservice.service;
 
+import com.microservice.inventoryservice.dto.InventoryResponse;
 import com.microservice.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +16,14 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCodes) {
+        return inventoryRepository.findBySkuCodeIn(skuCodes)
+                .stream()
+                .map(inventory ->
+                    InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity() > 0)
+                            .build()
+                ).collect(Collectors.toList());
     }
 }
